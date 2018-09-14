792a2f8749a8cf754c0e81255124e1eb3c80edd0
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-09-14 13:55:22 -0400

    Added store controller and views

---
+++ music-store/Controllers/StoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace music_store.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Store/Product
+        public ActionResult Product(String ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
