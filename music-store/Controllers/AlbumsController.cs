a4bcf30f86e216a27c84f4a57d62995cea7cbaf3
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-09-21 13:37:30 -0400

    Added albums model

---
+++ music-store/Controllers/AlbumsController.cs
@@ -0,0 +1,28 @@
+﻿using music_store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace music_store.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Albums
+        public ActionResult Index()
+        {
+            //create a mocklist of type album//
+            var albums = new List<Album>(); 
+
+            for (int i=1; i <= 10; i++)
+            {
+                albums.Add(new Album { Title = "Album " + i.ToString() });
+            }
+
+            //add albums to ViewBag for display//
+            ViewBag.albums = albums;
+            return View();
+        }
+    }
+}
